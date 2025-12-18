aef4a735858f8688d1c013bc51673e3be1ed3c1b
Kentypa <12orator12@gmail.com>
2025-12-18 14:59:01 -0800

    added new section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(function(){
-
+$(function () {
+  $(".grid").imagesLoaded(function () {
+    $(".grid").masonry({
+      itemSelector: ".grid-item",
+      horizontalOrder: true,
+      fitWidth: true,
+      gutter: 30,
+    });
+  });
 });
